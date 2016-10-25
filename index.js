29b407539406fc312ad78d2f110a2338f4165389
Xinkai Wang <aakwxk@hotmail.com>
2016-10-25 00:36:07 -0400

    ifconfig-parser.js added

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict'
+
+// var fs = require('fs');
+// var parser = require('./ifconfig-parser');
+
+// fs.readFile('./example.ifconfig','utf8', function(err, data) {
+//     console.dir(parser(data));
+// });
+
+var executeCmd = require('./executeCmd');
+
+executeCmd().then(console.dir);
